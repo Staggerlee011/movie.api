f9429796f94dd93e581299197b35412d04bd6723
Stephen Bennett <stebennettsjb@gmail.com>
2020-01-01 22:03:34 +0000

    init

---
+++ Repositiories/MovieRepository.cs
@@ -0,0 +1,100 @@
+﻿using Dapper;
+using MovieApp.Models;
+using Npgsql;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieApp.Repositiories
+{
+    public class MovieRepository : IMovieRepository
+    {
+        private readonly ConnectionString _connectionString;
+
+        public MovieRepository(ConnectionString connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public async Task<int> AddMovie(CreateMovieModel movie)
+        {
+            const string query = @"INSERT INTO movies (moviesName, directorsId, releaseYear)
+                                   VALUES(@Name, @DirectorId, @ReleaseYear)";
+
+            using (var conn = new NpgsqlConnection(_connectionString.Value))
+            {
+                var result = await conn.ExecuteAsync(
+                    query,
+                    new { Name = movie.Name, DirectorId = movie.DirectorId, ReleaseYear = movie.ReleaseYear });
+                return result;
+            }
+        }
+
+        public async Task<IEnumerable<DirectorModel>> GetAllDirectors()
+        {
+            const string query = @"SELECT d.directorsId ,
+                                    d.directorsName ,
+                                    m.directorsId ,
+                                    m.moviesId ,
+                                    m.moviesName MovieName ,
+                                    m.releaseYear
+                                FROM directors d
+                                INNER JOIN movies m ON d.directorsId = m.directorsId";
+
+            using (var conn = new NpgsqlConnection(_connectionString.Value))
+            {
+                var directorDictionary = new Dictionary<int, DirectorModel>();
+
+                var result = await conn.QueryAsync<DirectorModel, DirectorMovie, DirectorModel>(
+                    query,
+                    (dir, mov) =>
+                    {
+                        if (!directorDictionary.TryGetValue(dir.Id, out DirectorModel director))
+                        {
+                            director = dir;
+                            director.Movies = new List<DirectorMovie>();
+                            directorDictionary.Add(director.Id, director);
+                        }
+                        director.Movies.Add(mov);
+                        return director;
+                    },
+                    splitOn: "directorId");
+
+                return result.Distinct();
+            }
+        }
+
+        public async Task<IEnumerable<MovieModel>> GetAllMovies()
+        {
+            const string query = @"SELECT m.moviesId,
+                                        m.moviesName,
+                                        d.directorsName AS DirectorName,
+                                        m.ReleaseYear
+                                    FROM movies m
+                                    INNER JOIN directors d ON m.directorsId = d.directorsId";
+
+            using (var conn = new NpgsqlConnection(_connectionString.Value))
+            {
+                var result = await conn.QueryAsync<MovieModel>(query);
+                return result;
+            }
+        }
+
+        public async Task<MovieModel> GetMovieById(int id)
+        {
+            const string query = @"SELECT m.moviesId ,
+                                    m.moviesName ,
+                                    d.directorsName AS DirectorName ,
+                                    m.releaseYear
+                                FROM movies m
+                                INNER JOIN directors d ON m.directorsId = d.directorsId
+                                WHERE m.moviesId = @Id";
+
+            using (var conn = new NpgsqlConnection(_connectionString.Value))
+            {
+                var result = await conn.QueryFirstOrDefaultAsync<MovieModel>(query, new { Id = id });
+                return result;
+            }
+        }
+    }
+}
---
+++ dockerfile
@@ -0,0 +1,23 @@
+FROM mcr.microsoft.com/dotnet/core/sdk:3.0 AS build
+WORKDIR /app
+
+# copy csproj and restore as distinct layers
+COPY *.sln .
+COPY *.csproj ./
+RUN dotnet restore
+
+# copy everything else and build app
+COPY ./ ./
+WORKDIR /app
+RUN dotnet publish -c Release -o out
+
+
+FROM mcr.microsoft.com/dotnet/core/aspnet:3.0 AS runtime
+WORKDIR /app
+COPY --from=build /app/out ./
+
+ENTRYPOINT ["dotnet", "/app/MovieApp.dll"]
+
+# ASP.NET Core: make sure views use the production versions
+ENV ASPNETCORE_ENVIRONMENT="Development"
+
